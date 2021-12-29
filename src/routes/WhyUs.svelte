3c9db084545c7a811cb247cc20ac65b19678e80b
Kiswono Prayogo <kiswono@gmail.com>
2021-12-29 22:07:46 +0700

    why 2

--- src/routes/WhyUs.svelte
+++ src/routes/WhyUs.svelte
@@ -12,7 +12,7 @@
             <div class="detail-box">
               <h5>Latest Best-Fit Technology</h5>
               <p>
-                We choose best fitting tech-stack, that are perfect fit for your use case, unlike other software house that using generalized tech-stack to solve everything. We do not conform to the popular/hyped tech (ReactJS, Kubernetes, PHP, Rust, RoR, etc), that are mostly will be bad in terms of operational cost, maintainability, performance and scalability.
+                We choose best fitting tech-stack, that are perfect fit for your use case, unlike other software house that using generalized tech-stack to solve everything. We do not conform to the popular/hyped tech (ReactJS, Kubernetes, PHP, Rust, RoR, etc), that are mostly will be bad in terms of operational cost or maintainability (developer's productivity) or performance and scalability.
               </p>
             </div>
           </div>
@@ -22,7 +22,7 @@
             <div class="detail-box">
               <h5>Flexible Pricing</h5>
               <p>
-                You can hire us by man-hour, or lump-sump project based. We only outsource the UI/UX and Graphics Design when necessary (and it would be transparent), all other development and architecture all will be fully implemented by our internal team (we are so proud on our own code quality, so we won't trust it to other party). We also provide training for your own team for further in-house development, so you won't get locked with us.
+                You can hire us by man-hour, or lump-sump project-based. We only outsource the UI/UX and Graphics Design when necessary (and it would be transparent), all other development and architecture all will be fully implemented by our internal team (we are so proud on our own code quality, so we won't trust it to other party). We also provide training for your own developer team for further in-house development, so you won't get locked with us.
               </p>
             </div>
           </div>
@@ -32,7 +32,7 @@
             <div class="detail-box">
               <h5>Experienced Developers</h5>
               <p>
-                Our team consist of experienced developers with 4-13 years of experience in software development and Information Technology, most projects we've done are in e-commerce, insurance, education, multimedia, and infrastructure sector. We use the most make-sense practice, in terms of maintainability and performance (best-practice for one company doesn't always best for other company).
+                Our team consist of experienced developers with 4-13 years of experience in software development and Information Technology, most projects we've done are in e-commerce, insurance, education, multimedia, and infrastructure sector. We use the most make-sense practice, in terms of maintainability and performance (because best-practice for one company doesn't always best for other company).
               </p>
             </div>
           </div>
