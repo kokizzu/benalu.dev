df0e24d4ec924a4d2c283f11b9de345cf312e91d
Kiswono Prayogo <kiswono@gmail.com>
2022-05-26 21:11:10 +0700

    remve delete rsync

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 
 npm run build
 find . -iname .DS_Store -exec rm {} \;
-rsync -ar --delete --progress public/ root@benalu.dev:/var/www/html 
+rsync -ar --progress public/ root@benalu.dev:/var/www/html 
 
