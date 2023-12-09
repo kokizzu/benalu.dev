77378f042cd43cedc24f463722440946ce6228de
Kiswono Prayogo <kiswono@gmail.com>
2023-12-10 00:23:49 +0700

    update gmap

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -84,7 +84,7 @@
 					<div class="map">
 						<div id="googleMap" style="margin:0 auto">
 							<iframe
-								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15771.632804497742!2d115.16379253331382!3d-8.794694848937166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd244a5742f9609%3A0xb64f9e3fb868978!2zOMKwNDgnMTIuNSJTIDExNcKwMTAnNDcuMyJF!5e0!3m2!1sen!2sid!4v1647093544223!5m2!1sen!2sid"
+								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d782.3536318002231!2d115.17966008313937!3d-8.8034425573462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2455241786bc9%3A0x88e50b6325a3a7e0!2sCV%20Bersyukur%20Nalar%20Luas!5e0!3m2!1sen!2sid!4v1702142548984!5m2!1sen!2sid"
 								width="100%" height="240" style="border:0;" allowfullscreen="" title="GPS coordinate: -8.803461, 115.179797"
 								loading="lazy"></iframe>
 						</div>
