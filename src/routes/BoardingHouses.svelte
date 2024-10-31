0a904de601365fc3af07bd673f1730a6a291ecda
Kiswono Prayogo <kiswono@gmail.com>
2024-10-31 20:04:45 +0700

    temp commit

--- src/routes/BoardingHouses.svelte
+++ src/routes/BoardingHouses.svelte
@@ -1,57 +1,141 @@
 <script>
-  import BoardingHouse from "../components/BoardingHouse.svelte";
-  import GoButton from "../components/GoButton.svelte";
+	import BoardingHouse from "../components/BoardingHouse.svelte";
+	import GoButton from "../components/GoButton.svelte";
 </script>
 
 <section id="BoardingHouses" class="client_section layout_padding my-4">
-  <div class="container">
-    <div class="heading_container pb-5">
+	<div class="container">
+		<div class="heading_container pb-5">
 			<h2 class="section-title">Boarding Houses</h2>
 		</div>
-    <div class="boarding_house_list">
-      <BoardingHouse
-        name="CV. Bersyukur Nalar Luas"
-        normalPrice={1400000}
-        discountPrice={1000000}
-        discountTerms="Must pay 50% before 15th September 2024"
-        facilities={[
-          "Wifi", "Field", "CCTV", "Water", "Swimming Pool", "Cafe",
+			<!-- TODO: make mapLink loading -->
+		<div class="boarding_house_list">
+			<BoardingHouse
+				mapLink="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d782.3536318002231!2d115.17966008313937!3d-8.8034425573462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2455241786bc9%3A0x88e50b6325a3a7e0!2sCV%20Bersyukur%20Nalar%20Luas!5e0!3m2!1sen!2sid!4v1702142548984!5m2!1sen!2sid"
+				name="Kost JC Dominion (D)"
+				facilities={[
+					"Wooden House",
+          "WiFi", "CCTV", "2 Shower", "Shared Trash Can", "Free Electricity (500K per month)",
         ]}
-        rooms={[
+				rooms={[
           {
-            name: "D101",
+            name: "D11",
             image_url: "/placeholder-image.webp",
-            size: "4x5",
-            availableAt: "2024-09-15",
-            facilities: ["WC", "Kitchen", "Cabinet", "Mattress"],
-            youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-          }, 
+            size: "3x4",
+            availableAt: "2024-11-15",
+            normalPrice: 1_400_000,
+        		discountPrice: 1_000_000,
+            facilities: [],
+             //youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+            // TODO: make youtube lazy loading
+          },
           {
-            name: "E201",
+            name: "D12",
             image_url: "/placeholder-image.webp",
-            size: "5x5",
-            availableAt: "2024-09-15",
-            facilities: ["Cabinet", "Kitchen", "Fan"],
+            size: "3x3",
+            availableAt: "2024-11-15",
+				normalPrice: 1_200_000,
+				discountPrice: 1_000_000,
+	         discountTerms: "Donate to our partner each month at least 100K",
+            facilities: [],
+             //youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+			 },
+          {
+            name: "G23",
+            image_url: "/placeholder-image.webp",
+            size: "3.5x3.5",
+            availableAt: "2024-11-15",
+				normalPrice: 1_200_000,
+				discountPrice: 1_000_000,
+	         discountTerms: "Donate to our partner each month at least 100K",
+            facilities: [],
+             //youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+          },
+          {
+            name: "G24",
+            image_url: "/placeholder-image.webp",
+            size: "3x3",
+            availableAt: "2024-10-30",
+				normalPrice: 1_200_000,
+				discountPrice: 1_000_000,
+	         discountTerms: "Donate to our partner each month at least 100K",
+            facilities: [],
+             //youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
           },
         ]}
-        mapLink="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d782.3536318002231!2d115.17966008313937!3d-8.8034425573462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2455241786bc9%3A0x88e50b6325a3a7e0!2sCV%20Bersyukur%20Nalar%20Luas!5e0!3m2!1sen!2sid!4v1702142548984!5m2!1sen!2sid"
-      />
-    </div>
-  </div>
-  <GoButton target="#Home"> Back to Top</GoButton>
+			/>
+			<BoardingHouse
+				mapLink="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d782.3536318002231!2d115.17966008313937!3d-8.8034425573462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2455241786bc9%3A0x88e50b6325a3a7e0!2sCV%20Bersyukur%20Nalar%20Luas!5e0!3m2!1sen!2sid!4v1702142548984!5m2!1sen!2sid"
+				name="Kost JC Grace (G)"
+				discountTerms="Donate to our partner each month at least 400K"
+				facilities={[
+					"Fibercement House",
+          "WiFi", "CCTV", "2 Shower", "Shared Trash Can", "Free Electricity",
+          "Portable Bed", "Lazy Chair", "Bedcover", "Bedsheet", "Topper", "Pillow", "Bolster", "2 Pillowcase", "2 Bolstercase", "KWS Meter", "Chair", "Adjustable Desk", "Doormat", "Mirror", "AC"
+        ]}
+				rooms={[
+          {
+            name: "G11",
+            image_url: "/placeholder-image.webp",
+            size: "4x4",
+            availableAt: "2024-10-20",
+            facilities: [],
+				normalPrice: 2_000_000,
+				discountPrice: 1_500_000,
+	         discountTerms: "Donate to our partner each month at least 400K",
+            //youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+          },
+          {
+            name: "G22",
+            image_url: "/placeholder-image.webp",
+            size: "3.5x3.5",
+            availableAt: "2024-10-30",
+				normalPrice: 1_500_000,
+				discountPrice: 1_000_000,
+	         discountTerms: "Donate to our partner each month at least 400K",
+            facilities: [],
+             //youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+          },
+          {
+            name: "G23",
+            image_url: "/placeholder-image.webp",
+            size: "3.5x3.5",
+            availableAt: "2024-10-30",
+				normalPrice: 1_500_000,
+				discountPrice: 1_000_000,
+	         discountTerms: "Donate to our partner each month at least 400K",
+            facilities: [],
+             //youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+          },
+          {
+            name: "G24",
+            image_url: "/placeholder-image.webp",
+            size: "3.5x3.5",
+            availableAt: "2024-10-30",
+				normalPrice: 1_500_000,
+				discountPrice: 1_000_000,
+	         discountTerms: "Donate to our partner each month at least 400K",
+            facilities: [],
+             //youtube_url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+          },
+        ]}
+			/>
+		</div>
+	</div>
+	<GoButton target="#Home"> Back to Top</GoButton>
 </section>
 
 <style>
-  .boarding_house_list {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 20px;
-    width: 100%;
-  }
+	.boarding_house_list {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 20px;
+		width: 100%;
+	}
 
-  @media only screen and (max-width : 768px) {
-    .boarding_house_list {
-      grid-template-columns: 1fr;
-    }
-  }
+	@media only screen and (max-width: 768px) {
+		.boarding_house_list {
+			grid-template-columns: 1fr;
+		}
+	}
 </style>
