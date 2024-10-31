e5fb7a3522478f57e191fa48508afd0ad00e9d90
Kiswono Prayogo <kiswono@gmail.com>
2024-10-31 20:47:55 +0700

    add more questions

--- kuisioner.js
+++ kuisioner.js
@@ -21,14 +21,31 @@ export const kuisioner = [
       "Tidak",
       "Selama mendapat ijin menginap dari pemilik kost",
       "Selama bukan lawan jenis",
+      "Selama sekeluarga",
     ]
   },
   {
-    "question": "Apakah etis merokok/mabuk2an/berisik di dalam kamar?",
+    "question": "Apakah etis merokok/mabuk2an di dalam kamar?",
     "choices": [
       "Tidak",
-      "Selama bukan di bangunan dengan bahan kayu",
-      "Kehilangan kesadaran di dalam kamar tidak apa2",
+      "Selama bukan di bangunan dengan bahan kayu atau yg mudah terbakar",
+      "Kehilangan kesadaran di dalam kamar tidak apa2, tidak ada yg terganggu",
+    ]
+  },
+  {
+    "question": "Apakah etis berisik di dalam kamar?",
+    "choices": [
+      "Tidak, mendengarkan musik harus menggunakan headset/headphone",
+      "Bernyanyi tidak apa2 selama merdu dan sesuai selera musik tetangga",
+      "Mengeraskan speaker tidak apa2, selama bukan jam tidur",
+    ]
+  },
+  {
+    "question": "Apabila ada kerusakan di dalam kamar atau fasilitas umum sebaiknya?",
+    "choices": [
+      "Diam saja sampai akhir kontrak/kost habis",
+      "Fitnah tetangga yg kemungkinan merusakkan hal tersebut",
+      "Segera melaporkan ke pemilik/penjaga kost agar diperbaiki",
     ]
   }
 ]
