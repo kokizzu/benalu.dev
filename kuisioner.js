b44ea4787111511229702049322f441b154235bc
Kiswono Prayogo <kiswono@gmail.com>
2024-10-31 23:24:58 +0700

    moreq question

--- kuisioner.js
+++ kuisioner.js
@@ -11,42 +11,51 @@ export const kuisioner = [
   {
     "question": "Sampah pribadi sebelum dibuang ke tempat sampah sebaiknya?",
     "choices": [
-      "Tunggu sampai membusuk/berbau/didatangi serangga",
       "Langsung saja dibuang tanpa dibungkus",
+      "Tunggu sampai membusuk/berbau/didatangi serangga",
       "Dimasukkan ke dalam kantong kresek/plastik terlebih dahulu",
     ]
   },
   {
     "question": "Apakah etis membawa teman/menginap ke dalam kamar?",
     "choices": [
-      "Tidak",
       "Boleh saja, selama tidak ketahuan pemilik kost",
+      "Hanya kalau sekeluarga",
       "Selama bukan lawan jenis",
-      "Selama sekeluarga",
+      "Tidak, karena pasti diusir oleh pemilik kost dan diblacklist",
     ]
   },
   {
     "question": "Apakah etis merokok/mabuk2an di dalam kamar?",
     "choices": [
-      "Tidak",
-      "Selama bukan di bangunan dengan bahan kayu atau yg mudah terbakar",
-      "Kehilangan kesadaran di dalam kamar tidak apa2, tidak ada yg terganggu",
+      "Selama bukan di bangunan dengan bahan kayu yg mudah terbakar",
+      "Kehilangan kesadaran di dalam kamar tidak apa2, kalau ada yg terganggu'pun tidak sadar",
+      "Tidak, punya disiplin kontrol diri itu baik buat masa depan",
     ]
   },
   {
     "question": "Apakah etis berisik di dalam kamar?",
     "choices": [
-      "Tidak, mendengarkan musik harus menggunakan headset/headphone",
-      "Bernyanyi tidak apa2 selama merdu dan sesuai selera musik tetangga",
       "Mengeraskan speaker tidak apa2, selama bukan jam tidur",
+      "Bernyanyi tidak apa2 selama merdu dan sesuai selera musik tetangga",
+      "Tidak, mendengarkan musik harus menggunakan headset/headphone",
     ]
   },
   {
     "question": "Apabila ada kerusakan di dalam kamar atau fasilitas umum sebaiknya?",
     "choices": [
       "Diam saja sampai akhir kontrak/kost habis atau ada yg lapor duluan",
-      "Fitnah tetangga yg kemungkinan merusakkan hal tersebut",
+      "Tuduh tetangga yg kemungkinan melakukan hal tersebut",
       "Segera melaporkan ke pemilik/penjaga kost agar diperbaiki agar tidak semakin parah",
+    ]
+  },
+  {
+    "question": "Manakah dari hal berikut ini yang baik untuk dilakukan",
+    "choices": [
+      "Memaku/melubangi dinding kamar",
+      "Membawa binatang peliharaan ke dalam kamar",
+      "Tidak mencuci kaki sebelum masuk ke dalam kamar",
+      "Membersihkan kamar dan mengganti sarung bantal/guling secara berkala",
     ]
   }
 ]
